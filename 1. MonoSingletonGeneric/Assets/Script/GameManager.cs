c2694002f25ee807e163fa0814169065b22cbbca
Shubham <40598861+vijit101@users.noreply.github.com>
2025-01-08 20:48:34 +0530

    Update GameManager.cs

--- 1. MonoSingletonGeneric/Assets/Script/GameManager.cs
+++ 1. MonoSingletonGeneric/Assets/Script/GameManager.cs
@@ -6,4 +6,9 @@ public class GameManager : MonoSingletongeneric<GameManager>
     // with this the Game Manager becomes a singleton and any other scripts will have 
     // Access to the Gamemanager.Instance as Isntance is static 
     // As Game manager is inheriting the monosingletongeneric so it becomes hence any script inheriting monosingleton generic will becoem a singleton 
+
+     protected override void Awake()
+        {
+            base.Awake();
+        }
 }
